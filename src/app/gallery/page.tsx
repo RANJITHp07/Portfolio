5a2e6d3d1b6ba5e260b2af386a598da72b39391e
ranjith <ranjithp27122003@gmail.com>
2025-07-18 01:51:56 +0530

    change in readme

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -15,20 +15,7 @@ export async function generateMetadata() {
 export default function Gallery() {
   return (
     <Flex maxWidth="l">
-      <Schema
-        as="webPage"
-        baseURL={baseURL}
-        title={gallery.title}
-        description={gallery.description}
-        path={gallery.path}
-        image={`/api/og/generate?title=${encodeURIComponent(gallery.title)}`}
-        author={{
-          name: person.name,
-          url: `${baseURL}${gallery.path}`,
-          image: `${baseURL}${person.avatar}`,
-        }}
-      />
-      <MasonryGrid />
+
     </Flex>
   );
 }
